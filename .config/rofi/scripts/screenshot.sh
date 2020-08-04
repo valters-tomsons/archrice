f8ce56567b75bbd1a5a5603ba5a9b4dba7e68d68
Valters <valters.tomsons@protonmail.com>
2020-08-04 13:22:38 +0300

    Mohave cursor update

--- .config/rofi/scripts/screenshot.sh
+++ .config/rofi/scripts/screenshot.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-## Author : Aditya Shakya (adi1090x)
-## Mail : adi1090x@gmail.com
-## Github : @adi1090x
-## Reddit : @adi1090x
+# Copy image to clipboard
+function copyimage {
+	IMAGE_TYPE=$(file -b --mime-type "$1")
+	xclip -selection clipboard -t "$IMAGE_TYPE" < "$1"
+}
 
 rofi_command="rofi -theme themes/screenshot.rasi"
 
@@ -14,17 +15,25 @@ window=""
 
 # Variable passed to rofi
 options="$screen\n$area\n$window"
-
 chosen="$(echo -e "$options" | $rofi_command -p '' -dmenu -selected-row 1)"
-case $chosen in
-    $screen)
-        sleep 1; scrot 'Screenshot_%Y-%m-%d-%S_$wx$h.png' -e 'mv $f $$(xdg-user-dir PICTURES)/Screenshots ; viewnior $$(xdg-user-dir PICTURES)/Screenshots/$f'
-        ;;
-    $area)
-        scrot -s 'Screenshot_%Y-%m-%d-%S_$wx$h.png' -e 'mv $f $$(xdg-user-dir PICTURES)/Screenshots ; viewnior $$(xdg-user-dir PICTURES)/Screenshots/$f'
-        ;;
-    $window)
-        sleep 1; scrot -u 'Screenshot_%Y-%m-%d-%S_$wx$h.png' -e 'mv $f $$(xdg-user-dir PICTURES)/Screenshots  ; viewnior $$(xdg-user-dir PICTURES)/Screenshots/$f'
-        ;;
-esac
 
+timestamp=$(date +'%d-%m-%Y_%T')
+
+case $chosen in 
+	$screen)
+		# sleep 1;
+		scrot 'Screenshot_%Y-%m-%d-%S_$wx$h.png' -e 'export file=$f';
+		echo $file;
+		# mv $f $(xdg-user-dir PICTURES)/Screenshots;
+	       	# copyimage $(xdg-user-dir PICTURES)/Screenshots/$f
+		;;
+	$area)
+		scrot -s 'Screenshot_%Y-%m-%d-%S_$wx$h.png' -e 'mv $f $$(xdg-user-dir PICTURES)/Screenshots ;
+		copyimage $$(xdg-user-dir PICTURES)/Screenshots/$f'
+		;;
+	$window)
+		sleep 1;
+		scrot -u 'Screenshot_%Y-%m-%d-%S_$wx$h.png' -e 'mv $f $$(xdg-user-dir PICTURES)/Screenshots ;
+		copyimage $$(xdg-user-dir PICTURES)/Screenshots/$f'
+		;;
+esac
